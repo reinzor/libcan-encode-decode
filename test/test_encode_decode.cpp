6d9da22d272196b933d50610faf05c148f43489c
Rein Appeldoorn <reinzor@gmail.com>
2016-07-25 23:39:19 +0200

    more unit tests

--- test/test_encode_decode.cpp
+++ test/test_encode_decode.cpp
@@ -5,6 +5,8 @@
 
 #include <stack>
 #include <ctime>
+#include <bitset>
+#include <cassert>
 
 std::stack<clock_t> tictoc_stack;
 unsigned int num_tests = 0;
@@ -29,6 +31,42 @@ inline void TEST(float k, float v)
         std::cerr << "Expected " << v << " but got " << k << std::endl;
         exit(1);
     }
+}
+
+inline void PRINT(uint64_t value, int64_t signed_value)
+{
+    std::cout << "Values signed vs unsigned:" << std::endl;
+    std::cout << std::bitset<64>(value) << ": " << value << std::endl;
+    std::cout << std::bitset<64>(signed_value) << ": " << signed_value << " (signed)" << std::endl;
+}
+
+inline void PRINT(uint8_t* array)
+{
+    std::cout << ">> Array:" << std::endl;
+    for (unsigned int i = 0; i < 8; ++i)
+        std::cout << "\t" << std::bitset<8>(array[i]) << std::endl;
+}
+
+inline void TEST_STORE_EXTRACT(int64_t value, unsigned int startbit, unsigned int length, bool is_big_endian, bool is_signed)
+{
+    // std::cout << "--\n TEST_STORE_EXTRACT(" << value << ", " << startbit << ", " << length << ", " << is_big_endian << ", " << is_signed << ")" << std::endl;
+    uint8_t src_array[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+
+    storeSignal(src_array, value, startbit, length, is_big_endian, is_signed);
+    TEST((int32_t) extractSignal(src_array, startbit, length, is_big_endian, is_signed), value);
+}
+
+inline void TEST_ENCODE_DECODE(float value, unsigned int startbit, unsigned int length, bool is_big_endian, bool is_signed, float factor, float offset)
+{
+    // std::cout << "--\n TEST_ENCODE_DECODE(" << value << ", " << startbit << ", " << length << ", " << is_big_endian << ", " << is_signed << ", " << factor << ", " << offset << ")" << std::endl;
+    uint8_t src_array[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+
+    // Only negative allowed when signed
+    assert(is_signed || (int64_t) fromPhysicalValue(value, factor, offset) >= 0);
+
+    encode(src_array, value, startbit, length, is_big_endian, is_signed, factor, offset);
+
+    TEST(decode(src_array, startbit, length, is_big_endian, is_signed, factor, offset), value);
 }
 
 int main()
@@ -118,22 +156,39 @@ int main()
         TEST(decode(src_array, 0, 8, false, false, 1.000000, 0), 12.000000);
     }
 
-    // Encode decode test
+    // Store extract
+    {       
+        TEST_STORE_EXTRACT(512, 8, 12, false, true);
+
+        // Limits
+        TEST_STORE_EXTRACT(512, 0, 32, false, false);
+        TEST_STORE_EXTRACT(512, 56, 32, true, false);
+        TEST_STORE_EXTRACT(512, 32, 32, false, false);
+        TEST_STORE_EXTRACT(512, 32, 32, true, false);
+
+        // Limits
+        TEST_STORE_EXTRACT(-512, 0, 32, false, true);
+        TEST_STORE_EXTRACT(-512, 56, 32, true, true);
+        TEST_STORE_EXTRACT(-512, 32, 32, false, true);
+        TEST_STORE_EXTRACT(-512, 32, 32, true, true);
+
+        // Some other
+        TEST_STORE_EXTRACT(26, 4, 8, false, false);
+        TEST_STORE_EXTRACT(26, 62, 8, true, false);
+        TEST_STORE_EXTRACT(-12, 4, 8, false, true);
+        TEST_STORE_EXTRACT(-13, 62, 8, true, true);
+        TEST_STORE_EXTRACT(-190, 62, 24, true, true);
+    }
+
+    // Encoding decoding
     {
-        // src, startbit, bitlength, is_big_endian, is_signed, factor, offset, name, value
-        uint8_t src_array[8];
+        TEST_ENCODE_DECODE(-1.0, 0, 7, false, true, 1.0, 0.0);
+        TEST_ENCODE_DECODE(-100.0, 0, 7, false, true, 100, 0.0);
+        TEST_ENCODE_DECODE(3.0, 56, 3, false, false, 1.000000, 0.0);
+        TEST_ENCODE_DECODE(35, 2, 6, true, false, 1.0, 0);
+        TEST_ENCODE_DECODE(1.0, 0, 2, true, false, 1.0, 0);
 
-        encode(src_array, -1.0, 24, 12, false, true, 0.010000, 0.000000); 
-        TEST(decode(src_array, 24, 12, false, true, 0.010000, 0.000000), -1.0); 
-
-        encode(src_array, 3.0, 56, 3, false, false, 1.000000, 0);
-        TEST(decode(src_array, 56, 3, false, false, 1.000000, 0), 3.000000);
-
-        encode(src_array, 35, 2, 6, true, false, 1.0, 0);
-        TEST(decode(src_array, 2, 6, true, false, 1.000000, 0), 35.000000);
-
-        encode(src_array, 1.0, 0, 2, true, false, 1.0, 0);
-        TEST(decode(src_array, 0, 2, true, false, 1.000000, 0), 1.000000);
+        TEST_ENCODE_DECODE(1.0, 62, 24, true, true, 0.1, 20);
     }
 
     toc();
