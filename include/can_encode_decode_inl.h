d8fee480e63ed099006eded601ca3b5ef6ba9504
Rein Appeldoorn <rein@ruvu.nl>
2018-02-04 15:25:40 +0100

    clang format

--- include/can_encode_decode_inl.h
+++ include/can_encode_decode_inl.h
@@ -1,114 +1,118 @@
 #ifndef CAN_ENCODE_DECODE_INL_H_
 #define CAN_ENCODE_DECODE_INL_H_
 
-#include <stdint.h> //uint typedefinitions, non-rtw!
+#include <stdint.h>  //uint typedefinitions, non-rtw!
 
-#define MASK64(nbits) ((0xffffffffffffffff)>> (64-nbits))
+#define MASK64(nbits) ((0xffffffffffffffff) >> (64 - nbits))
 
 inline float toPhysicalValue(uint64_t target, float factor, float offset, bool is_signed)
 {
-    if (is_signed)
-        return ( (int32_t) target ) * factor + offset;
-    else
-        return target * factor + offset;
+  if (is_signed)
+    return ((int32_t)target) * factor + offset;
+  else
+    return target * factor + offset;
 }
 
 inline uint64_t fromPhysicalValue(float physical_value, float factor, float offset)
 {
-    return (physical_value - offset) / factor;
+  return (physical_value - offset) / factor;
 }
 
-inline void storeSignal(uint8_t* frame, uint64_t value, const uint8_t startbit, const uint8_t length, bool is_big_endian, bool is_signed)
+inline void storeSignal(uint8_t* frame, uint64_t value, const uint8_t startbit, const uint8_t length,
+                        bool is_big_endian, bool is_signed)
 {
-    uint8_t start_byte = startbit / 8;
-    uint8_t startbit_in_byte = startbit % 8;
-    uint8_t end_byte = 0;
-    int8_t count = 0;
-    uint8_t current_target_length = (8-startbit_in_byte);
+  uint8_t start_byte = startbit / 8;
+  uint8_t startbit_in_byte = startbit % 8;
+  uint8_t end_byte = 0;
+  int8_t count = 0;
+  uint8_t current_target_length = (8 - startbit_in_byte);
 
-    // Mask the value
-    value &= MASK64(length);
+  // Mask the value
+  value &= MASK64(length);
 
-    // Write bits of startbyte
-    frame[start_byte] |= value << startbit_in_byte;
+  // Write bits of startbyte
+  frame[start_byte] |= value << startbit_in_byte;
 
-    // Write residual bytes
-    if(is_big_endian) // Motorola (big endian)
+  // Write residual bytes
+  if (is_big_endian)  // Motorola (big endian)
+  {
+    end_byte = (start_byte * 8 + 8 - startbit_in_byte - length) / 8;
+
+    for (count = start_byte - 1; count >= end_byte; count--)
     {
-        end_byte = (start_byte * 8 + 8 - startbit_in_byte - length) / 8;
-
-        for(count = start_byte-1; count >= end_byte; count --)
-        {
-            frame[count] |= value >> current_target_length;
-            current_target_length += 8;
-        }
+      frame[count] |= value >> current_target_length;
+      current_target_length += 8;
     }
-    else // Intel (little endian)
+  }
+  else  // Intel (little endian)
+  {
+    end_byte = (startbit + length - 1) / 8;
+
+    for (count = start_byte + 1; count <= end_byte; count++)
     {
-        end_byte = (startbit + length - 1) / 8;
-
-        for(count = start_byte+1; count <= end_byte; count ++)
-        {
-            frame[count] |= value >> current_target_length;
-            current_target_length += 8;
-        }
+      frame[count] |= value >> current_target_length;
+      current_target_length += 8;
     }
+  }
 }
 
-inline uint64_t extractSignal(const uint8_t* frame, const uint8_t startbit, const uint8_t length, bool is_big_endian, bool is_signed)
+inline uint64_t extractSignal(const uint8_t* frame, const uint8_t startbit, const uint8_t length, bool is_big_endian,
+                              bool is_signed)
 {
-    uint8_t start_byte = startbit / 8;
-    uint8_t startbit_in_byte = startbit % 8;
-    uint8_t current_target_length = (8-startbit_in_byte);
-    uint8_t end_byte = 0;
-    int8_t count = 0;
+  uint8_t start_byte = startbit / 8;
+  uint8_t startbit_in_byte = startbit % 8;
+  uint8_t current_target_length = (8 - startbit_in_byte);
+  uint8_t end_byte = 0;
+  int8_t count = 0;
 
-    // Write first bits to target
-    uint64_t target = frame[start_byte] >> startbit_in_byte;
-    
-    // Write residual bytes 
-    if(is_big_endian) // Motorola (big endian)
+  // Write first bits to target
+  uint64_t target = frame[start_byte] >> startbit_in_byte;
+
+  // Write residual bytes
+  if (is_big_endian)  // Motorola (big endian)
+  {
+    end_byte = (start_byte * 8 + 8 - startbit_in_byte - length) / 8;
+
+    for (count = start_byte - 1; count >= end_byte; count--)
     {
-        end_byte = (start_byte * 8 + 8 - startbit_in_byte - length) / 8;
-
-        for(count = start_byte-1; count >= end_byte; count --)
-        {
-            target |= frame[count] << current_target_length;
-            current_target_length += 8;
-        }
+      target |= frame[count] << current_target_length;
+      current_target_length += 8;
     }
-    else // Intel (little endian)
+  }
+  else  // Intel (little endian)
+  {
+    end_byte = (startbit + length - 1) / 8;
+
+    for (count = start_byte + 1; count <= end_byte; count++)
     {
-        end_byte = (startbit + length - 1) / 8;
-
-        for(count = start_byte+1; count <= end_byte; count ++)
-        {
-            target |= frame[count] << current_target_length;
-            current_target_length += 8;
-        }
+      target |= frame[count] << current_target_length;
+      current_target_length += 8;
     }
+  }
 
-    // Mask value
-    target &= MASK64(length);
+  // Mask value
+  target &= MASK64(length);
 
-    // perform sign extension
-    if (is_signed)
-    {    
-        int64_t msb_sign_mask = 1 << (length - 1);
-        target = ( (int32_t) target ^ msb_sign_mask) - msb_sign_mask;
-    }
+  // perform sign extension
+  if (is_signed)
+  {
+    int64_t msb_sign_mask = 1 << (length - 1);
+    target = ((int32_t)target ^ msb_sign_mask) - msb_sign_mask;
+  }
 
-    return target;
+  return target;
 }
 
-inline float decode(const uint8_t* frame, const uint16_t startbit, const uint16_t length, bool is_big_endian, bool is_signed, float factor, float offset)
+inline float decode(const uint8_t* frame, const uint16_t startbit, const uint16_t length, bool is_big_endian,
+                    bool is_signed, float factor, float offset)
 {
-    return toPhysicalValue(extractSignal(frame, startbit, length, is_big_endian, is_signed), factor, offset, is_signed);
+  return toPhysicalValue(extractSignal(frame, startbit, length, is_big_endian, is_signed), factor, offset, is_signed);
 }
 
-inline void encode(uint8_t* frame, const float value, const uint16_t startbit, const uint16_t length, bool is_big_endian, bool is_signed, float factor, float offset)
+inline void encode(uint8_t* frame, const float value, const uint16_t startbit, const uint16_t length,
+                   bool is_big_endian, bool is_signed, float factor, float offset)
 {
-    storeSignal(frame, fromPhysicalValue(value, factor, offset), startbit, length, is_big_endian, is_signed);
+  storeSignal(frame, fromPhysicalValue(value, factor, offset), startbit, length, is_big_endian, is_signed);
 }
 
 #endif
--- test/test_encode_decode.cpp
+++ test/test_encode_decode.cpp
@@ -1,7 +1,7 @@
 #include "../include/can_encode_decode_inl.h"
 
 #include <iostream>
-#include <stdlib.h> 
+#include <stdlib.h>
 
 #include <stack>
 #include <ctime>
@@ -11,186 +11,190 @@
 std::stack<clock_t> tictoc_stack;
 unsigned int num_tests = 0;
 
-void tic() 
+void tic()
 {
-    tictoc_stack.push(clock());
+  tictoc_stack.push(clock());
 }
 
-void toc() 
+void toc()
 {
-    std::cout << "All " << num_tests << " tests passed within " << ((double)(clock() - tictoc_stack.top())) / CLOCKS_PER_SEC << " seconds!" << std::endl;
-    tictoc_stack.pop();
+  std::cout << "All " << num_tests << " tests passed within "
+            << ((double)(clock() - tictoc_stack.top())) / CLOCKS_PER_SEC << " seconds!" << std::endl;
+  tictoc_stack.pop();
 }
 
 inline void TEST(float k, float v)
 {
-    num_tests++;
-    bool near = (k >= v-0.01 && k <= v+0.01);
-    if(!near)
-    {
-        std::cerr << "Expected " << v << " but got " << k << std::endl;
-        exit(1);
-    }
+  num_tests++;
+  bool near = (k >= v - 0.01 && k <= v + 0.01);
+  if (!near)
+  {
+    std::cerr << "Expected " << v << " but got " << k << std::endl;
+    exit(1);
+  }
 }
 
 inline void PRINT(uint64_t value, int64_t signed_value)
 {
-    std::cout << "Values signed vs unsigned:" << std::endl;
-    std::cout << std::bitset<64>(value) << ": " << value << std::endl;
-    std::cout << std::bitset<64>(signed_value) << ": " << signed_value << " (signed)" << std::endl;
+  std::cout << "Values signed vs unsigned:" << std::endl;
+  std::cout << std::bitset<64>(value) << ": " << value << std::endl;
+  std::cout << std::bitset<64>(signed_value) << ": " << signed_value << " (signed)" << std::endl;
 }
 
 inline void PRINT(uint8_t* array)
 {
-    std::cout << ">> Array:" << std::endl;
-    for (unsigned int i = 0; i < 8; ++i)
-        std::cout << "\t" << std::bitset<8>(array[i]) << std::endl;
+  std::cout << ">> Array:" << std::endl;
+  for (unsigned int i = 0; i < 8; ++i)
+    std::cout << "\t" << std::bitset<8>(array[i]) << std::endl;
 }
 
-inline void TEST_STORE_EXTRACT(int64_t value, unsigned int startbit, unsigned int length, bool is_big_endian, bool is_signed)
+inline void TEST_STORE_EXTRACT(int64_t value, unsigned int startbit, unsigned int length, bool is_big_endian,
+                               bool is_signed)
 {
-    // std::cout << "--\n TEST_STORE_EXTRACT(" << value << ", " << startbit << ", " << length << ", " << is_big_endian << ", " << is_signed << ")" << std::endl;
-    uint8_t src_array[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+  // std::cout << "--\n TEST_STORE_EXTRACT(" << value << ", " << startbit << ", " << length << ", " << is_big_endian <<
+  // ", " << is_signed << ")" << std::endl;
+  uint8_t src_array[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
 
-    storeSignal(src_array, value, startbit, length, is_big_endian, is_signed);
-    TEST((int32_t) extractSignal(src_array, startbit, length, is_big_endian, is_signed), value);
+  storeSignal(src_array, value, startbit, length, is_big_endian, is_signed);
+  TEST((int32_t)extractSignal(src_array, startbit, length, is_big_endian, is_signed), value);
 }
 
-inline void TEST_ENCODE_DECODE(float value, unsigned int startbit, unsigned int length, bool is_big_endian, bool is_signed, float factor, float offset)
+inline void TEST_ENCODE_DECODE(float value, unsigned int startbit, unsigned int length, bool is_big_endian,
+                               bool is_signed, float factor, float offset)
 {
-    // std::cout << "--\n TEST_ENCODE_DECODE(" << value << ", " << startbit << ", " << length << ", " << is_big_endian << ", " << is_signed << ", " << factor << ", " << offset << ")" << std::endl;
-    uint8_t src_array[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+  // std::cout << "--\n TEST_ENCODE_DECODE(" << value << ", " << startbit << ", " << length << ", " << is_big_endian <<
+  // ", " << is_signed << ", " << factor << ", " << offset << ")" << std::endl;
+  uint8_t src_array[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
 
-    // Only negative allowed when signed
-    assert(is_signed || (int64_t) fromPhysicalValue(value, factor, offset) >= 0);
+  // Only negative allowed when signed
+  assert(is_signed || (int64_t)fromPhysicalValue(value, factor, offset) >= 0);
 
-    encode(src_array, value, startbit, length, is_big_endian, is_signed, factor, offset);
+  encode(src_array, value, startbit, length, is_big_endian, is_signed, factor, offset);
 
-    TEST(decode(src_array, startbit, length, is_big_endian, is_signed, factor, offset), value);
+  TEST(decode(src_array, startbit, length, is_big_endian, is_signed, factor, offset), value);
 }
 
 int main()
 {
-    tic();
+  tic();
 
-    // Decode test 1 (big endian unsigned)
-    {
-        uint64_t src = 0x182000110008d;
-        uint8_t src_array[8] = {141, 0, 16, 1, 0, 130, 1, 0};
+  // Decode test 1 (big endian unsigned)
+  {
+    uint64_t src = 0x182000110008d;
+    uint8_t src_array[8] = { 141, 0, 16, 1, 0, 130, 1, 0 };
 
-        TEST(extractSignal(src_array, 0, 2, true, false), 1);
-        TEST(decode(src_array, 0, 2, true, false, 1.000000, 0), 1.000000);
+    TEST(extractSignal(src_array, 0, 2, true, false), 1);
+    TEST(decode(src_array, 0, 2, true, false, 1.000000, 0), 1.000000);
 
-        TEST(extractSignal(src_array, 2, 6, true, false), 35);
-        TEST(decode(src_array, 2, 6, true, false, 1.000000, 0), 35.000000);
+    TEST(extractSignal(src_array, 2, 6, true, false), 35);
+    TEST(decode(src_array, 2, 6, true, false, 1.000000, 0), 35.000000);
 
-        TEST(extractSignal(src_array, 21, 11, true, false), 0);
-        TEST(decode(src_array, 21, 11, true, false, 0.100000, 0), 0.000000);
+    TEST(extractSignal(src_array, 21, 11, true, false), 0);
+    TEST(decode(src_array, 21, 11, true, false, 0.100000, 0), 0.000000);
 
-        TEST(extractSignal(src_array, 25, 12, true, false), 2048);
-        TEST(decode(src_array, 25, 12, true, false, 0.062500, -128), 0.000000);
+    TEST(extractSignal(src_array, 25, 12, true, false), 2048);
+    TEST(decode(src_array, 25, 12, true, false, 0.062500, -128), 0.000000);
 
-        TEST(extractSignal(src_array, 32, 9, true, false), 256);
-        TEST(decode(src_array, 32, 9, true, false, 0.062500, -16), 0.000000);
+    TEST(extractSignal(src_array, 32, 9, true, false), 256);
+    TEST(decode(src_array, 32, 9, true, false, 0.062500, -16), 0.000000);
 
-        TEST(extractSignal(src_array, 48, 3, true, false), 1);
-        TEST(decode(src_array, 48, 3, true, false, 1.000000, 0), 1.000000);
+    TEST(extractSignal(src_array, 48, 3, true, false), 1);
+    TEST(decode(src_array, 48, 3, true, false, 1.000000, 0), 1.000000);
 
-        TEST(extractSignal(src_array, 51, 3, true, false), 0);
-        TEST(decode(src_array, 51, 3, true, false, 1.000000, 0), 0.000000);
+    TEST(extractSignal(src_array, 51, 3, true, false), 0);
+    TEST(decode(src_array, 51, 3, true, false, 1.000000, 0), 0.000000);
 
-        TEST(extractSignal(src_array, 54, 10, true, false), 520);
-        TEST(decode(src_array, 54, 10, true, false, 0.100000, -52), 0.000000);
+    TEST(extractSignal(src_array, 54, 10, true, false), 520);
+    TEST(decode(src_array, 54, 10, true, false, 0.100000, -52), 0.000000);
 
-        TEST(extractSignal(src_array, 56, 3, true, false), 0);
-        TEST(decode(src_array, 56, 3, true, false, 1.000000, 0), 0.000000);
+    TEST(extractSignal(src_array, 56, 3, true, false), 0);
+    TEST(decode(src_array, 56, 3, true, false, 1.000000, 0), 0.000000);
 
-        TEST(extractSignal(src_array, 59, 3, true, false), 0);
-        TEST(decode(src_array, 59, 3, true, false, 1.000000, 0), 0.000000);
+    TEST(extractSignal(src_array, 59, 3, true, false), 0);
+    TEST(decode(src_array, 59, 3, true, false, 1.000000, 0), 0.000000);
 
-        TEST(extractSignal(src_array, 62, 2, true, false), 0);
-        TEST(decode(src_array, 62, 2, true, false, 1.000000, 0), 0.000000);
-    }
+    TEST(extractSignal(src_array, 62, 2, true, false), 0);
+    TEST(decode(src_array, 62, 2, true, false, 1.000000, 0), 0.000000);
+  }
 
-    // Decode test 2 (little endian, signed and unsigned)
-    {
-        // src, startbit, bitlength, is_big_endian, is_signed, factor, offset, name, value
-        uint8_t src_array[8] = {12, 0, 5, 112, 3, 205, 31, 131};
+  // Decode test 2 (little endian, signed and unsigned)
+  {
+    // src, startbit, bitlength, is_big_endian, is_signed, factor, offset, name, value
+    uint8_t src_array[8] = { 12, 0, 5, 112, 3, 205, 31, 131 };
 
-        TEST(extractSignal(src_array, 60, 2, false, false), 0);
-        TEST(decode(src_array, 60, 2, false, true, 1.000000, 0), 0.000000);
+    TEST(extractSignal(src_array, 60, 2, false, false), 0);
+    TEST(decode(src_array, 60, 2, false, true, 1.000000, 0), 0.000000);
 
-        TEST(extractSignal(src_array, 55, 1, false, false), 0);
-        TEST(decode(src_array, 55, 1, false, false, 1.000000, 0), 0.000000);
+    TEST(extractSignal(src_array, 55, 1, false, false), 0);
+    TEST(decode(src_array, 55, 1, false, false, 1.000000, 0), 0.000000);
 
-        TEST(extractSignal(src_array, 20, 4, false, false), 0);
-        TEST(decode(src_array, 20, 4, false, false, 1.000000, 0), 0.000000);
+    TEST(extractSignal(src_array, 20, 4, false, false), 0);
+    TEST(decode(src_array, 20, 4, false, false, 1.000000, 0), 0.000000);
 
-        TEST(extractSignal(src_array, 62, 2, false, false), 2);
-        TEST(decode(src_array, 62, 2, false, false, 1.000000, 0), 2.000000);
+    TEST(extractSignal(src_array, 62, 2, false, false), 2);
+    TEST(decode(src_array, 62, 2, false, false, 1.000000, 0), 2.000000);
 
-        TEST(extractSignal(src_array, 34, 3, false, false), 0);
-        TEST(decode(src_array, 34, 3, false, false, 1.000000, 0), 0.000000);
+    TEST(extractSignal(src_array, 34, 3, false, false), 0);
+    TEST(decode(src_array, 34, 3, false, false, 1.000000, 0), 0.000000);
 
-        TEST(extractSignal(src_array, 37, 3, false, false), 0);
-        TEST(decode(src_array, 37, 3, false, false, 1.000000, 0), 0.000000);
+    TEST(extractSignal(src_array, 37, 3, false, false), 0);
+    TEST(decode(src_array, 37, 3, false, false, 1.000000, 0), 0.000000);
 
-        TEST(extractSignal(src_array, 59, 1, false, true), 0);
-        TEST(decode(src_array, 59, 1, false, true, 1.000000, 0), 0.000000);
+    TEST(extractSignal(src_array, 59, 1, false, true), 0);
+    TEST(decode(src_array, 59, 1, false, true, 1.000000, 0), 0.000000);
 
-        TEST(extractSignal(src_array, 56, 3, false, false), 3);
-        TEST(decode(src_array, 56, 3, false, false, 1.000000, 0), 3.000000);
+    TEST(extractSignal(src_array, 56, 3, false, false), 3);
+    TEST(decode(src_array, 56, 3, false, false, 1.000000, 0), 3.000000);
 
-        TEST(extractSignal(src_array, 52, 3, false, false), 1);
-        TEST(decode(src_array, 52, 3, false, false, 1.000000, 0), 1.000000);
+    TEST(extractSignal(src_array, 52, 3, false, false), 1);
+    TEST(decode(src_array, 52, 3, false, false, 1.000000, 0), 1.000000);
 
-        TEST(extractSignal(src_array, 8, 12, false, false), 1280);
-        TEST(decode(src_array, 8, 12, false, false, 0.062500, 0), 80.000000);
+    TEST(extractSignal(src_array, 8, 12, false, false), 1280);
+    TEST(decode(src_array, 8, 12, false, false, 0.062500, 0), 80.000000);
 
-        TEST(decode(src_array, 40, 12, false, true, 0.062500, 0), -3.187500);
+    TEST(decode(src_array, 40, 12, false, true, 0.062500, 0), -3.187500);
 
-        TEST(extractSignal(src_array, 24, 10, false, true), (uint64_t) -144);
-        TEST(decode(src_array, 24, 10, false, true, 0.062500, 0), -9.000000);
+    TEST(extractSignal(src_array, 24, 10, false, true), (uint64_t)-144);
+    TEST(decode(src_array, 24, 10, false, true, 0.062500, 0), -9.000000);
 
-        TEST(extractSignal(src_array, 0, 8, false, false), 12);
-        TEST(decode(src_array, 0, 8, false, false, 1.000000, 0), 12.000000);
-    }
+    TEST(extractSignal(src_array, 0, 8, false, false), 12);
+    TEST(decode(src_array, 0, 8, false, false, 1.000000, 0), 12.000000);
+  }
 
-    // Store extract
-    {       
-        TEST_STORE_EXTRACT(512, 8, 12, false, true);
+  // Store extract
+  {
+    TEST_STORE_EXTRACT(512, 8, 12, false, true);
 
-        // Limits
-        TEST_STORE_EXTRACT(512, 0, 32, false, false);
-        TEST_STORE_EXTRACT(512, 56, 32, true, false);
-        TEST_STORE_EXTRACT(512, 32, 32, false, false);
-        TEST_STORE_EXTRACT(512, 32, 32, true, false);
+    // Limits
+    TEST_STORE_EXTRACT(512, 0, 32, false, false);
+    TEST_STORE_EXTRACT(512, 56, 32, true, false);
+    TEST_STORE_EXTRACT(512, 32, 32, false, false);
+    TEST_STORE_EXTRACT(512, 32, 32, true, false);
 
-        // Limits
-        TEST_STORE_EXTRACT(-512, 0, 32, false, true);
-        TEST_STORE_EXTRACT(-512, 56, 32, true, true);
-        TEST_STORE_EXTRACT(-512, 32, 32, false, true);
-        TEST_STORE_EXTRACT(-512, 32, 32, true, true);
+    // Limits
+    TEST_STORE_EXTRACT(-512, 0, 32, false, true);
+    TEST_STORE_EXTRACT(-512, 56, 32, true, true);
+    TEST_STORE_EXTRACT(-512, 32, 32, false, true);
+    TEST_STORE_EXTRACT(-512, 32, 32, true, true);
 
-        // Some other
-        TEST_STORE_EXTRACT(26, 4, 8, false, false);
-        TEST_STORE_EXTRACT(26, 62, 8, true, false);
-        TEST_STORE_EXTRACT(-12, 4, 8, false, true);
-        TEST_STORE_EXTRACT(-13, 62, 8, true, true);
-        TEST_STORE_EXTRACT(-190, 62, 24, true, true);
-    }
+    // Some other
+    TEST_STORE_EXTRACT(26, 4, 8, false, false);
+    TEST_STORE_EXTRACT(26, 62, 8, true, false);
+    TEST_STORE_EXTRACT(-12, 4, 8, false, true);
+    TEST_STORE_EXTRACT(-13, 62, 8, true, true);
+    TEST_STORE_EXTRACT(-190, 62, 24, true, true);
+  }
 
-    // Encoding decoding
-    {
-        TEST_ENCODE_DECODE(-1.0, 0, 7, false, true, 1.0, 0.0);
-        TEST_ENCODE_DECODE(-100.0, 0, 7, false, true, 100, 0.0);
-        TEST_ENCODE_DECODE(3.0, 56, 3, false, false, 1.000000, 0.0);
-        TEST_ENCODE_DECODE(35, 2, 6, true, false, 1.0, 0);
-        TEST_ENCODE_DECODE(1.0, 0, 2, true, false, 1.0, 0);
+  // Encoding decoding
+  {
+    TEST_ENCODE_DECODE(-1.0, 0, 7, false, true, 1.0, 0.0);
+    TEST_ENCODE_DECODE(-100.0, 0, 7, false, true, 100, 0.0);
+    TEST_ENCODE_DECODE(3.0, 56, 3, false, false, 1.000000, 0.0);
+    TEST_ENCODE_DECODE(35, 2, 6, true, false, 1.0, 0);
+    TEST_ENCODE_DECODE(1.0, 0, 2, true, false, 1.0, 0);
 
-        TEST_ENCODE_DECODE(1.0, 62, 24, true, true, 0.1, 20);
-    }
+    TEST_ENCODE_DECODE(1.0, 62, 24, true, true, 0.1, 20);
+  }
 
-    toc();
+  toc();
 }
-
